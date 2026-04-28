9513af354911f86bc23baa9f24fd4e79691e658d
Felipe Lima <felipehenl122@gmail.com>
2026-04-28 19:12:46 -0300

    feat: my solutions for C# fundamental exercises

---
+++ section-05/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bank account;
+            Console.WriteLine("Enter your account number.");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Log in as account holder.");
+            string holder = Console.ReadLine();
+            Console.WriteLine("Will there be an initial deposit (y/n)?");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 'y' || resp == 'Y')
+            {
+                Console.WriteLine("Enter the initial deposit amount:");
+                double depositAmount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                account = new bank(number, holder, depositAmount);
+
+            }
+            else
+            {
+                account = new bank(number, holder);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Account details:");
+            Console.WriteLine(account);
+            Console.WriteLine();
+            Console.WriteLine("Enter a deposit amount:");
+            double amount = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            account.Deposit(amount);
+            Console.WriteLine("Updated account details:");
+            Console.WriteLine(account);
+
+            Console.WriteLine();
+            Console.WriteLine("Enter an amount to withdraw: ");
+            amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            account.Withdraw(amount);
+            Console.WriteLine("Updated account details:");
+            Console.WriteLine(account);
+            
+
+        }
+    }
+}
---
+++ section-05/bank.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ex1
+{
+    class bank
+    {
+        public int Number { get; private set; }
+        public string Holder { get; set; }
+        public double Balance { get; private set; }
+
+
+        public bank(int number, string holder)
+        {
+            Number = number;
+            Holder = holder;
+        }
+
+
+        public bank(int number, string holder, double depositAmount) : this(number, holder)
+        {
+            Deposit(depositAmount);
+        }
+
+
+
+
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Balance -= amount + 5.0;
+        }
+
+        public override string ToString()
+        {
+            return $"Account {Number}, Holder: {Holder}, Balance: $ {Balance.ToString("F2", CultureInfo.InvariantCulture)}"; 
+        }
+       
+    }
+}
